a2699f65dc2cd89ed303e197d3080408b14d5613
rogergraves <rogergraves@gmail.com>
2014-06-25 21:54:59 -0700

    Added PicksViewController, connected Next button in NewDecisionViewController to it.

---
+++ Popinator/PicksViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PicksViewController.swift
+//  Popinator
+//
+//  Created by Roger Graves on 6/25/14.
+//  Copyright (c) 2014 Wahanegi, Inc. All rights reserved.
+//
+
+import UIKit
+
+class PicksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
